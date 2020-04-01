9a04bbf7877fc343d436fa518c11ea706404eabb
Dan Cojocar <dan.cojocar@gmail.com>
2020-04-01 20:31:13 +0300

    Lecture #6

--- lectures/6/ButtonDemo/app/src/main/java/com/example/buttondemo/MainActivity.kt
+++ lectures/6/ButtonDemo/app/src/main/java/com/example/buttondemo/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.buttondemo
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import com.google.android.things.contrib.driver.button.Button
+import com.google.android.things.pio.PeripheralManager
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
+import java.util.*
 
 
 /**
@@ -29,22 +33,25 @@ import java.io.IOException
 class MainActivity : Activity() {
 
   private lateinit var mButton: Button
-  private val gpioPinName = "GPIO6_IO14"
+  private val gpioPinName = "BCM2"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
+    logd("GPIO ports: " + PeripheralManager.getInstance().gpioList)
+    logd("Just press the button!")
     try {
       mButton = Button(
         gpioPinName,
-        // high signal indicates the button is pressed
-        // use with a pull-down resistor
-        Button.LogicState.PRESSED_WHEN_HIGH
+        Button.LogicState.PRESSED_WHEN_LOW
       )
       mButton.setOnButtonEventListener { _, _ ->
         // do something awesome
         logd("Something awesome!")
+        val rnd = Random()
+        val color =
+          Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+        panel.setBackgroundColor(color)
       }
     } catch (e: IOException) {
       loge("Unable to register the button :(")
--- lectures/6/ButtonDemo/app/src/main/java/com/example/buttondemo/Utils.kt
+++ lectures/6/ButtonDemo/app/src/main/java/com/example/buttondemo/Utils.kt
@@ -3,9 +3,9 @@ package com.example.buttondemo
 import android.util.Log
 
 fun Any.logd(message: String = "No message!", cause: Throwable? = null) {
-    Log.d(this.javaClass.simpleName, message, cause)
+  Log.d(this.javaClass.simpleName, message, cause)
 }
 
 fun Any.loge(message: String = "No message!", cause: Throwable? = null) {
-    Log.e(this.javaClass.simpleName, message, cause)
+  Log.e(this.javaClass.simpleName, message, cause)
 }
